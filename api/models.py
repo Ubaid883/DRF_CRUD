8611082441e14928d85bd4b0bb4165b9b9fd1a15
ubaid <ubaidsaleem825@gmail.com>
2025-06-26 08:34:02 +0500

    create student model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=55)
+    email = models.EmailField(max_length=55)
+    department = models.CharField(max_length=55)
+    reg_number = models.IntegerField()
+    address = models.CharField(max_length=255)
+    major_subject = models.CharField(max_length=55, choices=[
+        ('Computer', 'Computer'),
+        ("Physics",'Physics'),
+        ("Math",'Math'),
+        ("Bio",'Bio'),
+    ])
