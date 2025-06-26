ed70c2a55832fb190b35e1e8119ddf5323107a88
ubaid <ubaidsaleem825@gmail.com>
2025-06-26 09:02:25 +0500

    git all student data

--- api/views.py
+++ api/views.py
@@ -3,13 +3,17 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
 # Create funcation based view here
+@api_view(['GET'])
 def student_view(request):
-    # get all student data
-    student = Student.objects.all()
-    # Convert complex data into json formate
-    serializer = StudentSerializer(student, many= True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        # get all student data
+        student = Student.objects.all()
+        # Convert complex data into json formate
+        serializer = StudentSerializer(student, many= True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     
